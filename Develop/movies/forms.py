cf31d1abcbd7f8bc8f61e1bafb9444cab9d447ab
Youhoseong <h0song@naver.com>
2021-05-25 15:36:04 +0900

    feat: movie evaluate function

--- Develop/movies/forms.py
+++ Develop/movies/forms.py
@@ -4,5 +4,4 @@ from django import forms
 
 class MovieEvalForm(forms.Form):
     
-    actionScore = forms.IntegerField(max_value = 5, min_value=1, label="액션 점수")
-    storyScore = forms.IntegerField(max_value=5, min_value=1, label="스토리 점수")
+    score = forms.IntegerField(max_value = 5, min_value=1, label="별점")
--- Develop/movies/views.py
+++ Develop/movies/views.py
@@ -1,15 +1,48 @@
 from django.shortcuts import render
 from django.views.generic import FormView, ListView
 from . import forms
+from . import models
+import random
 from django.shortcuts import render, redirect, reverse
 # Create your views here.
 
 class MovieEvalView(FormView):
     template_name = "movie/movie_eval.html"
-    form_class = forms.MovieEvalForm
+    keyword_list = []
+    recommended_movie = []
 
+    def recommender(self):
+        duplicate_keyword_delete_list = list(set(self.keyword_list))
+        recommended_keyword_list = []
+
+        for keyword in duplicate_keyword_delete_list:
+            if self.keyword_list.count(keyword) >= 1:
+                recommended_keyword_list.append(keyword)
+        recommended_movie_list = models.MovieModel.objects.filter(keyword__in = recommended_keyword_list)
+
+        return recommended_movie_list
+
+        
     def post(self, request):
-        actionScore = request.POST.get("actionScore")
-        print(actionScore)
+        movie_id = request.POST.get("id")
+        print(movie_id)
+        score = request.POST.get("score")
+        print(score)
+
+        if int(score) > 3:
+            movie_obj = models.MovieModel.objects.get(id=movie_id)
+            print(movie_obj)
+            self.keyword_list.append(movie_obj.keyword) 
+        print(self.keyword_list)
+
+        if len(self.keyword_list) > 5:
+
+            self.recommended_movie = self.recommender()
+            print(self.recommended_movie)
         return redirect(reverse("movies:evaluate-form"))
 
+    def get(self, request):
+        form_class = forms.MovieEvalForm
+        movie_model = models.MovieModel.objects.all()
+        random_items = random.sample(list(movie_model),5)
+        return render(request, "movie/movie_eval.html", {"movies": random_items, "form": form_class, "recommend": self.recommended_movie})
