9e4a93fd177f1da2e01326bc7ee2b7c52e3fbedf
wnsgud0428 <wnsgud0428@cau.ac.kr>
2021-05-25 12:29:32 +0900

    12331

--- Develop/movies/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class MovieModel(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=40)
-    director = models.CharField(max_length=10)
-    keyword = ArrayField()
