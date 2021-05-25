99f8d2aed345d6d3b2692425bfcada392ad754e8
Youhoseong <h0song@naver.com>
2021-05-25 12:27:13 +0900

    feat: movie model

--- Develop/movies/admin.py
+++ Develop/movies/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from . import models
 # Register your models here.
+
+@admin.register(models.MovieModel)
+class MovieAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("title","director","keyword")}
+        ),
+
+    )
